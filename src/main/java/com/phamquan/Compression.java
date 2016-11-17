1c65a79abb8cba37872a40ac28b132b2bc1472cf
= <phamquan1105@gmail.com>
2016-11-17 14:32:42 +0700

    Inittal project Image compressor

---
+++ src/main/java/com/phamquan/Compression.java
@@ -0,0 +1,55 @@
+package com.phamquan;
+
+import org.imgscalr.Scalr;
+
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.awt.image.*;
+
+import javax.imageio.*;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
+import javax.swing.*;
+
+
+class Compression {
+
+    public static void main (String [] args) {
+
+        compressImage("image.png", "output", ".jpg", 500, 500, 1.0f);
+    }
+
+    public static void compressImage (String srcPath, String destPath, String format, int with, int height, float quality) {
+
+        try {
+            File input = new File(srcPath);
+            BufferedImage image = ImageIO.read(input);
+
+            image = Scalr.resize(image, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.FIT_TO_WIDTH, with, height, Scalr.OP_ANTIALIAS);
+            File compressedImageFile = new File(destPath + format );
+            OutputStream os =new FileOutputStream(compressedImageFile);
+
+            Iterator<ImageWriter>writers =  ImageIO.getImageWritersByFormatName("jpg");
+            ImageWriter writer = writers.next();
+
+            ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+
+            writer.setOutput(ios);
+
+            ImageWriteParam iwp = writer.getDefaultWriteParam();
+
+            iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            iwp.setCompressionQuality(quality);
+            writer.write(null, new IIOImage(image, null, null), iwp);
+
+            os.close();
+            ios.close();
+            writer.dispose();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
